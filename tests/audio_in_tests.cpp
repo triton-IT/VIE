6e3aba26d9d088f2ac5008785f04f13129c2f9a6
Régis Ramillien <r.ramillien@tritonit.tech>
2023-02-03 23:29:28 +0100

    Updated unit tests

--- tests/audio_in_tests.cpp
+++ tests/audio_in_tests.cpp
@@ -1,9 +1,9 @@
-#include "../src/components/audio_input.hpp"
+#include "../src/modules/audio_input.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
 using namespace live::tritone::vie;
-using namespace live::tritone::vie::processor::component;
+using namespace live::tritone::vie::processor::module;
 
 SCENARIO("audio_in should output same sample than input one.", "[audio-in]") {
     GIVEN("An empty input buffer") {
@@ -14,22 +14,22 @@ SCENARIO("audio_in should output same sample than input one.", "[audio-in]") {
 
         output_process_data data;
 
-        std::array<float_array_component_output*, 32> outputs;
+        std::array<float_array_module_output*, 32> outputs;
         for (auto output : outputs)
         {
-            output = new float_array_component_output;
+            output = new float_array_module_output;
         }
 		
         WHEN("input is processed") {
             audio_in.process(data);
 
             THEN("audio output slot value is empty") {
-                auto output_components = reinterpret_cast<std::array<component_output*, 32>&>(outputs);
+                auto output_modules = reinterpret_cast<std::array<module_output*, 32>&>(outputs);
 				
-                auto nb_values = audio_in.get_output_values(1, output_components);
+                auto nb_values = audio_in.get_output_values(1, output_modules);
 				
                 REQUIRE(nb_values == 1);
-                REQUIRE(output_components[0]->to_float_array().nb_values == 0);
+                REQUIRE(output_modules[0]->to_float_array().nb_values == 0);
             }
         }
     }
@@ -48,26 +48,26 @@ SCENARIO("audio_in should output same sample than input one.", "[audio-in]") {
         data.num_outputs = 1;
         data.num_samples = 8;
 
-        std::array<float_array_component_output*, 32> outputs;
+        std::array<float_array_module_output*, 32> modules;
         for (int i = 0; i <32; i++)
         {
-            outputs[i] = new float_array_component_output;
+            modules[i] = new float_array_module_output;
         }
 
         WHEN("input is processed") {
             audio_in.process(data);
 
             THEN("audio output slot value contains same data as input") {
-                auto output_components = reinterpret_cast<std::array<component_output*, 32>&>(outputs);
+                auto output_modules = reinterpret_cast<std::array<module_output*, 32>&>(modules);
 
-                auto nb_values = audio_in.get_output_values(1, output_components);
+                auto nb_values = audio_in.get_output_values(1, output_modules);
 
                 REQUIRE(nb_values == 1);
-                REQUIRE(output_components[0]->to_float_array().nb_values == 8);
-                REQUIRE(output_components[0]->to_float_array().values[0] == 0.0f);
-                REQUIRE(output_components[0]->to_float_array().values[1] == 1.0f);
-                REQUIRE(output_components[0]->to_float_array().values[2] == 2.0f);
-                REQUIRE(output_components[0]->to_float_array().values[3] == 3.0f);
+                REQUIRE(output_modules[0]->to_float_array().nb_values == 8);
+                REQUIRE(output_modules[0]->to_float_array().values[0] == 0.0f);
+                REQUIRE(output_modules[0]->to_float_array().values[1] == 1.0f);
+                REQUIRE(output_modules[0]->to_float_array().values[2] == 2.0f);
+                REQUIRE(output_modules[0]->to_float_array().values[3] == 3.0f);
             }
         }
     }
